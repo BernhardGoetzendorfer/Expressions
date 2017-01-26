bc06d7d4adc4f28a844d0190bfeb96b6f72b1717
Kanevry <bernhardgoetzendorfer95@gmail.com>
2017-01-26 16:56:46 +0100

    initial commit

---
+++ Expressions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (true ^ false) //XOR Expression, eins von beiden muss true sein, damit aaaa ausgegeben wird
+            {
+                Console.WriteLine("aaaa");
+            }
+
+            object x = 3;
+
+
+            var test = (float)(int)x;
+
+            dynamic a = 1;
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
